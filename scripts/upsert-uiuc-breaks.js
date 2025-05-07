cd383bd01e1e8fe7eb178a1f1b30c57dd1fad141
Kevin Yu <kevinyu567@gmail.com>
2025-05-07 13:24:42 -0400

    Refactor breaks creation to use separate `createMany`, insert UIUC

--- scripts/upsert-uiuc-breaks.js
+++ scripts/upsert-uiuc-breaks.js
@@ -5,11 +5,11 @@ const { PrismaClient, BreakType } = require('@prisma/client');
     const prisma = new PrismaClient();
 
     // Should be a valid name defined in `schoolConfigs`.
-    const schoolName = 'Purdue';
+    const schoolName = 'UIUC';
 
     const schoolData = {
         schoolName,
-        airportIatas: ['IND', 'ORD']
+        airportIatas: ['ORD', 'MDW']
     }
 
     const school = await prisma.school.upsert({
@@ -25,11 +25,10 @@ const { PrismaClient, BreakType } = require('@prisma/client');
 
     const breaks = await prisma.break.createMany({
         data: [
-            { startDate: new Date('2025-10-11'), endDate: new Date('2025-10-14'), breakType: BreakType.Fall, schoolID: school.id },
-            { startDate: new Date('2025-11-26'), endDate: new Date('2025-11-30'), breakType: BreakType.Thanksgiving, schoolID: school.id },
-            { startDate: new Date('2025-12-20'), endDate: new Date('2026-01-11'), breakType: BreakType.Winter, schoolID: school.id },
+            { startDate: new Date('2025-11-22'), endDate: new Date('2025-11-30'), breakType: BreakType.Fall, schoolID: school.id },
+            { startDate: new Date('2025-12-19'), endDate: new Date('2026-01-19'), breakType: BreakType.Winter, schoolID: school.id },
             { startDate: new Date('2026-03-14'), endDate: new Date('2026-03-22'), breakType: BreakType.Spring, schoolID: school.id },
-            { startDate: new Date('2026-05-09'), endDate: new Date('2026-08-23'), breakType: BreakType.Summer, schoolID: school.id },
+            { startDate: new Date('2026-05-15'), endDate: new Date('2026-08-23'), breakType: BreakType.Summer, schoolID: school.id },
         ]
     })
     console.log(breaks);
